7f0adce27d1662f20700cc9e134c5ce73ae67519
Rando Tomingas <randotm@users.noreply.github.com>
2017-06-13 22:56:16 +0300

    Timer now placed at center of the page-ish.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var pomodoroCount = 0
 testEnd = new Date(1497378493231);
+var timer;
 
 window.onload = function() {
-	var timer = window.setInterval(function() {
-		document.getElementById("timer").innerHTML = getTimeRemaining(testEnd);
+	timer = window.setInterval(function() {
+		document.getElementById("minutes").innerHTML = getTimeRemaining(testEnd)[0];
+		document.getElementById("seconds").innerHTML = getTimeRemaining(testEnd)[1];
+		document.getElementById("milliseconds").innerHTML = getTimeRemaining(testEnd)[2];
+		console.log(getTimeRemaining(testEnd)[2]);
 	}, 1);
 };
 
@@ -15,15 +19,24 @@ function getEndTime(minutes) {
 }
 
 function getTimeRemaining(endTime) {
+	var timeArray = [];
 	var now = new Date();
 	var timeLeft = new Date(endTime - now);
-	var timeLeftString = addZeroBefore(timeLeft.getMinutes(), 1) + ":" + addZeroBefore(timeLeft.getSeconds(), 1) + "." + addZeroBefore(timeLeft.getMilliseconds(), 2);
-	return timeLeftString
+	var timeLeftString = 
+	timeArray.push(addZeroBefore(timeLeft.getMinutes(), 2));
+	timeArray.push(addZeroBefore(timeLeft.getSeconds(), 2));
+	if (timeLeft.getMilliseconds() != 1000) {
+		timeArray.push(addZeroBefore(timeLeft.getMilliseconds(), 3));
+	} else {
+		timeArray.push("999");
+	}
+	return timeArray;
 }
 
 function addZeroBefore(number, amount) {
 	if(number < (10**amount) && number > -(10**amount)) {
-		var prefix = ((10**amount) + " ");
+		number = number.toString();
+		var prefix = (10 ** (amount - number.length)) + " ";
 		prefix = prefix.slice(1,-1);
 		number = prefix + parseInt(number);
 	}
